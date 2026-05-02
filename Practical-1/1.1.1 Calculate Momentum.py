e1b15164090c02186b0878e754a129cc4bde0206
sarveshagni44 <sarveshagni07@gmail.com>
2026-05-02 18:06:26 +0530

    Update 1.1.1 Calculate Momentum.py

--- Practical-1/1.1.1 Calculate Momentum.py
+++ Practical-1/1.1.1 Calculate Momentum.py
@@ -1,15 +1,5 @@
-#Write a program that accepts the mass of an object (in kilograms) and its velocity (in meters per second),then calculates and displays the momentum of the object. The momentum is calculated using the formula:
-
-#Input Format:
-
-#A single floating-point number representing the mass of the object in kilograms.
-#A single floating-point number representing the velocity of the object in meters per second.
-
-#Output Format:
-
-#The output will display calculated momentum with appropriate units (kgm/s) (rounded up to 2 decimal places).//
-
-#code :
+#Write a program that accepts the mass of an object and its velocity,then calculates and displays the momentum of the object.
+#code
 
 m=float(input())
 v=float(input())

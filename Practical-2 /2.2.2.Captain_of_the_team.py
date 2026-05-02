1bdab4a4333e35ea6acadce88471ff8149331f61
sarveshagni44 <sarveshagni07@gmail.com>
2026-05-02 19:17:16 +0530

    Create 2.2.2.Captain_of_the_team.py

---
+++ Practical-2 /2.2.2.Captain_of_the_team.py
@@ -0,0 +1,5 @@
+#You are provided with the heights of 11 cricket players (in centimeters). Your task is to identify the tallest player, who will be selected as the captain of the team.
+#code :
+
+hight=list(map(int,input().split()))
+print(max(hight))
